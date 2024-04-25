7e65d4339d095afe9bdd87cd2d64e88e7f8d5a5d
Satvik1769 <manchandasatvik@gmail.com>
2024-04-25 18:44:39 +0530

    game

---
+++ ball_game/target/debug/deps/rust-fd12710f8c418d3d.d
@@ -0,0 +1,5 @@
+/home/satvik/Desktop/rust/target/debug/deps/rust-fd12710f8c418d3d: src/main.rs
+
+/home/satvik/Desktop/rust/target/debug/deps/rust-fd12710f8c418d3d.d: src/main.rs
+
+src/main.rs:
---
+++ ball_game/target/debug/rust.d
@@ -0,0 +1,1 @@
+/home/satvik/Desktop/rust/target/debug/rust: /home/satvik/Desktop/rust/src/main.rs
